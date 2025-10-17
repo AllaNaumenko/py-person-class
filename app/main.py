1dac0ac70dd720815daa82076e8273db3ac9a5f1
Your Name <allanaumenko@Allas-MacBook-Air.local>
2025-10-17 10:07:56 +0800

    Added Person class and create_person_list function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age 
+        Person.people[name] = self
+
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.people.clear()
+    result = []
+    for p in people:
+        person = Person(p["name"], p["age"])
+        result.append(people)
+    for p in people:
+        me = Person.peolpe[p["name"]]
+        if "wife" in p and p["wifw"] is not None:
+            setattr(me, "wifw", Person.people[p["wife"]])
+        if "husband" in p and p["husband"] is not None:
+            setattr(me, "husband", Person.people[p["husband"]])
+
+    return result
